74b68646a5ee519ce7b48de0a63e757c963a075b
alsiesta <as@daslizenzatelier.de>
2024-04-17 18:50:18 +0200

    added post request

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from rest_framework import permissions, viewsets
-from .models import Todo
+# Pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring
 from .serializer import TodoSerializer
-
+from django.http import HttpResponse
+from rest_framework import viewsets
+from django.core import serializers
+from .models import Todo
 
 class TodoViewSet(viewsets.ModelViewSet):
     """
@@ -10,4 +11,17 @@ class TodoViewSet(viewsets.ModelViewSet):
     """
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
-    permission_classes = []  # permissions.IsAuthenticated
+    permission_classes = [] # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(title= request.POST.get('title', 'Mein Titel'), 
+                                  description= request.POST.get('description', 'Was für eine Beschreibung'),
+                                  user= request.user,
+                                )
+        serialized_obj = serializers.serialize('json', [todo, ]) 
+        return HttpResponse(serialized_obj, content_type='application/json')
+
+
+
+    
+
